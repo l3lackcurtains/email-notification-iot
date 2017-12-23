b6e69b17645f723e767176d1cf0635d0fc2a8803
Madhav Poudel <king.ov.universe@gmail.com>
2017-12-23 13:57:05 +0545

    some changes to know client is connected

--- index.js
+++ index.js
@@ -62,6 +62,9 @@ const notifier = require('mail-notifier')
 
 function emailListener() {
 	io.on('connection', async (socket) => {
+		socket.on('entry', (data) => {
+			console.log(data, " has been connected.")
+		})
 		// on we have active clients
 		io.clients((error, clients) => {
 			if (error) console.log('Error due to clients.', error)
@@ -69,56 +72,52 @@ function emailListener() {
 			console.log('New Client: ' + socket.id)
 			console.log('Total Clients: ', clients)
 			console.log('*****************************************************************')
+		})
 
-			socket.on('iamin', (data) => {
-				console.log(data, " has been connected.")
-			})
-
-			User.findOne({}, 'email password', function(err, data) {
-				if(!!data) {
-					const imap = {
-						user: data.email,
-						password: data.password,
-						host: "imap.gmail.com",
-						port: 993, 
-						tls: true,
-						tlsOptions: { rejectUnauthorized: false }
-					}
-					const n = notifier(imap)
-					n.on('end', () => n.start())
-						.on('mail', mail => {
-							Email.find({}, 'email', (err, data) => {
-								!!data && !err && data.map(async (d) => {
-									if(d.email === mail.from[0].address) {
-										console.log('Email received from ' + mail.from[0].address)
-										const newInbox = Inbox({
-											from: mail.headers.from,
-											date: mail.date,
-											subject: mail.headers.subject,
-											body: mail.html,
-										})
-										await newInbox.save()
-										const inboxData = {
-											from: mail.headers.from,
-											subject: mail.headers.subject,
-											date: mail.date,
-											body: mail.html,
-										}
-										io.clients((error, cls) => {
-											if(cls.length > 0) {
-												cls.map((c) => {
-													console.log('Email sent to client ' + c)
-													io.to(c).emit('newemail', { mail: inboxData })
-												})
-											}
-										})
+		User.findOne({}, 'email password', function(err, data) {
+			if(!!data) {
+				const imap = {
+					user: data.email,
+					password: data.password,
+					host: "imap.gmail.com",
+					port: 993, 
+					tls: true,
+					tlsOptions: { rejectUnauthorized: false }
+				}
+				const n = notifier(imap)
+				n.on('end', () => n.start())
+					.on('mail', mail => {
+						Email.find({}, 'email', (err, data) => {
+							!!data && !err && data.map(async (d) => {
+								if(d.email === mail.from[0].address) {
+									console.log('Email received from ' + mail.from[0].address)
+									const newInbox = Inbox({
+										from: mail.headers.from,
+										date: mail.date,
+										subject: mail.headers.subject,
+										body: mail.html,
+									})
+									await newInbox.save()
+									const inboxData = {
+										from: mail.headers.from,
+										subject: mail.headers.subject,
+										date: mail.date,
+										body: mail.html,
 									}
-								})
+									io.clients((error, cls) => {
+										if(cls.length > 0) {
+											cls.map((c) => {
+												console.log('Email sent to client ' + c)
+												io.to(c).emit('newemail', { mail: inboxData })
+											})
+										}
+									})
+								}
 							})
-						}).start()	
-					
-					}
-				})
+						})
+					}).start()	
+				
+				}
 			})
 		})
 }
