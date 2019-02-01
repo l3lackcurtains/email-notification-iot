76fb7766de4ed32bf34559a0e62a264b27fbc933
Madhav Poudel <king.ov.universe@gmail.com>
2019-02-01 21:42:00 +0545

    Readme changes

---
+++ README.md
@@ -0,0 +1,19 @@
+## Broadmail Server
+
+Notify you and view email whenever you got new email from your client. This application was created for a internet service provider of Nepal, BroadLink.
+
+Built with NodeJS and Socket.
+
+### Run in development mode
+
+```bash
+yarn
+yarn dev
+```
+
+
+### Run in Production Mode
+```bash
+yarn
+yarn start
+```
--- index.js
+++ index.js
@@ -1,136 +1,146 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var helmet = require('helmet')
-var Imap = require('imap'),
-		inspect = require('util').inspect
+var express = require("express");
+var bodyParser = require("body-parser");
+var helmet = require("helmet");
 
-var mongoose = require('mongoose')
-var passport = require('passport')
-var Promise = require('bluebird')
-var app = express()
+var mongoose = require("mongoose");
+var passport = require("passport");
+var Promise = require("bluebird");
+var app = express();
 
-var server = require('http').Server(app)
+var server = require("http").Server(app);
 
-var io = require('socket.io')(server)
+var io = require("socket.io")(server);
 
-var User = require('./models/user')
-var Message = require('./models/message')
-var Inbox = require('./models/inbox')
-var Email = require('./models/email')
+var User = require("./models/user");
+var Message = require("./models/message");
+var Inbox = require("./models/inbox");
+var Email = require("./models/email");
 
-var user = require('./routes/user')
-var email = require('./routes/email')
-var inbox = require('./routes/inbox')
+var user = require("./routes/user");
+var email = require("./routes/email");
+var inbox = require("./routes/inbox");
 
-var config = require('./utils/config')
+var config = require("./utils/config");
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: false, parameterLimit: 50000 }))
-app.use(helmet())
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: false,
+    parameterLimit: 50000
+  })
+);
+app.use(helmet());
 
-mongoose.Promise = Promise
+mongoose.Promise = Promise;
 // Setup Mongoose connection with mongoDB
-mongoose.connect(process.env.MDB)
-const db = mongoose.connection
-db.on('error', () => console.log('Failed to connect to Database.'))
-	.once('open', () => {
-		console.log('Connected to Database.')
-		emailListener()
-	})
+mongoose.connect(process.env.MDB);
+const db = mongoose.connection;
+db.on("error", () => console.log("Failed to connect to Database.")).once(
+  "open",
+  () => {
+    console.log("Connected to Database.");
+    emailListener();
+  }
+);
 // Using bluebird Promise removes depricated warning
 
 // Middlewares
-app.use(passport.initialize())
+app.use(passport.initialize());
 
 // Require passport
-require('./utils/passport')(passport)
+require("./utils/passport")(passport);
 
-app.get('/', (req, res) => {
-	res.send('This is entry point')
-})
+app.get("/", (req, res) => {
+  res.send("This is entry point");
+});
 
-app.use('/api', user)
-app.use('/api', email)
-app.use('/api', inbox)
+app.use("/api", user);
+app.use("/api", email);
+app.use("/api", inbox);
 
 server.listen(port, () => {
-	console.log(`Listening to ${port}`)
-})
+  console.log(`Listening to ${port}`);
+});
 
-const notifier = require('mail-notifier')
-
+const notifier = require("mail-notifier");
 
 function emailListener() {
-	io.on('connection', (socket) => {
-		socket.on('entry', (data) => {
-			console.log(data, "[" ,socket.id, "]", "has been connected.")
-		})
+  io.on("connection", socket => {
+    socket.on("entry", data => {
+      console.log(data, "[", socket.id, "]", "has been connected.");
+    });
 
-		socket.on('error', (error) => {
-			console.log('Socket Error: ', error)
-		})
+    socket.on("error", error => {
+      console.log("Socket Error: ", error);
+    });
 
-		// reconnect everyday
-		setTimeout(() => socket.disconnect(true), 86400000)
+    // reconnect everyday
+    setTimeout(() => socket.disconnect(true), 86400000);
 
-		// on we have active clients
-		io.clients((error, clients) => {
-			if (error) console.log('Error due to clients.', error)
-			console.log('*****************************************************************')
-			console.log('New Client: ' + socket.id)
-			console.log('Total Clients: ', clients)
-			console.log('*****************************************************************')
-		})
-		User.findOne({}, 'email password', function(err, data) {
-			if(!!data) {
-				const imap = {
-					user: data.email,
-					password: data.password,
-					host: "imap.gmail.com",
-					port: 993, 
-					tls: true,
-					tlsOptions: { rejectUnauthorized: false }
-				}
-				const n = notifier(imap)
-						n.on('end', () => n.start())
-							.on('mail', mail => {
-								Email.find({}, 'email', (err, data) => {
-									!!data && !err && data.map(async (d) => {
-										if(d.email === mail.from[0].address) {
-											console.log('Email received from ' + mail.from[0].address)
-											const newInbox = Inbox({
-												from: mail.headers.from,
-												date: mail.date,
-												subject: mail.headers.subject,
-												body: mail.html,
-											})
-											await newInbox.save()
-											const inboxData = {
-												from: mail.headers.from,
-												subject: mail.headers.subject,
-												date: mail.date,
-												body: mail.html,
-											}
-											io.clients((error, cls) => {
-												if(cls.length > 0) {
-													cls.map((c) => {
-														console.log('Email sent to client ' + c)
-														io.to(c).emit('newemail', { mail: inboxData })
-													})
-												}
-											})
-										}
-									})
-								})
-							})
-							.on('error', e => {
-								console.log("Error: ", e)
-							})
-							.start()
-				
-			}
-		})
-	})
+    // on we have active clients
+    io.clients((error, clients) => {
+      if (error) console.log("Error due to clients.", error);
+      console.log(
+        "*****************************************************************"
+      );
+      console.log("New Client: " + socket.id);
+      console.log("Total Clients: ", clients);
+      console.log(
+        "*****************************************************************"
+      );
+    });
+    User.findOne({}, "email password", function(err, data) {
+      if (!!data) {
+        const imap = {
+          user: data.email,
+          password: data.password,
+          host: "imap.gmail.com",
+          port: 993,
+          tls: true,
+          tlsOptions: { rejectUnauthorized: false }
+        };
+        const n = notifier(imap);
+        n.on("end", () => n.start())
+          .on("mail", mail => {
+            Email.find({}, "email", (err, data) => {
+              !!data &&
+                !err &&
+                data.map(async d => {
+                  if (d.email === mail.from[0].address) {
+                    console.log("Email received from " + mail.from[0].address);
+                    const newInbox = Inbox({
+                      from: mail.headers.from,
+                      date: mail.date,
+                      subject: mail.headers.subject,
+                      body: mail.html
+                    });
+                    await newInbox.save();
+                    const inboxData = {
+                      from: mail.headers.from,
+                      subject: mail.headers.subject,
+                      date: mail.date,
+                      body: mail.html
+                    };
+                    io.clients((error, cls) => {
+                      if (cls.length > 0) {
+                        cls.map(c => {
+                          console.log("Email sent to client " + c);
+                          io.to(c).emit("newemail", { mail: inboxData });
+                        });
+                      }
+                    });
+                  }
+                });
+            });
+          })
+          .on("error", e => {
+            console.log("Error: ", e);
+          })
+          .start();
+      }
+    });
+  });
 }
